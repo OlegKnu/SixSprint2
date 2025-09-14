c57dc152f74fe9d8f559cdebd6a0a432e41b482d
Daniil, Kholkin <danyakholkin@yandex.ru>
2025-09-14 21:54:06 +0300

    4 commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,9 +1,8 @@
-// internal/handlers/handlers.go
 package handlers
 
 import (
+	"html/template"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -12,62 +11,65 @@ import (
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
 )
 
-type Handler struct {
-	Logger *log.Logger
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, "Ошибка загрузки шаблона: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Ошибка отображения шаблона: "+err.Error(), http.StatusInternalServerError)
+	}
 }
 
-func (h *Handler) Index(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
-}
-
-func (h *Handler) Upload(w http.ResponseWriter, r *http.Request) {
-	// 1. Парсим форму с лимитом 10 МБ
-	if err := r.ParseMultipartForm(10 << 20); err != nil {
-		h.Logger.Println("Ошибка парсинга формы:", err)
-		http.Error(w, "Ошибка парсинга формы", http.StatusInternalServerError)
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
 		return
 	}
 
-	file, header, err := r.FormFile("file")
+	err := r.ParseMultipartForm(10 << 20)
 	if err != nil {
-		h.Logger.Println("Ошибка получения файла из формы:", err)
-		http.Error(w, "Ошибка загрузки файла", http.StatusInternalServerError)
+		http.Error(w, "Ошибка парсинга формы: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	file, header, err := r.FormFile("uploadfile")
+	if err != nil {
+		http.Error(w, "Ошибка получения файла из формы: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
 	data, err := io.ReadAll(file)
 	if err != nil {
-		h.Logger.Println("Ошибка чтения файла:", err)
-		http.Error(w, "Ошибка чтения файла", http.StatusInternalServerError)
+		http.Error(w, "Ошибка чтения файла: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	converted, err := service.Convert(string(data))
+	result, err := service.Convert(string(data))
 	if err != nil {
-		h.Logger.Println("Ошибка конвертации:", err)
-		http.Error(w, "Ошибка конвертации", http.StatusInternalServerError)
+		http.Error(w, "Ошибка конвертации: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	timeStr := time.Now().UTC().Format("20060102T150405Z")
 	ext := filepath.Ext(header.Filename)
-	filename := timeStr + ext
+	filename := time.Now().UTC().Format("20060102_150405") + ext
 
 	outFile, err := os.Create(filename)
 	if err != nil {
-		h.Logger.Println("Ошибка создания файла:", err)
-		http.Error(w, "Ошибка сохранения файла", http.StatusInternalServerError)
+		http.Error(w, "Ошибка создания файла: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer outFile.Close()
 
-	if _, err := outFile.WriteString(converted); err != nil {
-		h.Logger.Println("Ошибка записи в файл:", err)
-		http.Error(w, "Ошибка записи файла", http.StatusInternalServerError)
+	_, err = outFile.WriteString(result)
+	if err != nil {
+		http.Error(w, "Ошибка записи в файл: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(converted))
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	_, _ = w.Write([]byte(result))
 }
